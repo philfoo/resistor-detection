9afe4012238ab589418820be719aec1d56f31459
john_dai <johnrundai@gmail.com>
2017-04-19 12:49:45 -0400

    Added edge_math to the main pipeline, made running main.py more configurable, tweaked sobel edge reading parameters, moved some methods to resistor_utilities

--- main.py
+++ main.py
@@ -1,42 +1,46 @@
 import cv2
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from crop_rectangle import cropRectangle
+from edge_math import edgeMath
+from resistor_utilities import initializeEllipticSnake
 from skimage.transform import rotate
 
-cap = cv2.VideoCapture(0)
-width = int(cap.get(3))
-height = int(cap.get(4))
 
-s = np.linspace(0, 2*np.pi, 400)
-# x = width/2 + width/3*np.cos(s)
-# y = height/2 + height/3*np.sin(s)
-x = 300 + 250*np.cos(s)
-y = 150 + 150*np.sin(s)
-init = np.array([x, y]).T
-init = init.astype(int)
-pts = init.reshape((-1, 1, 2))
-initImg = np.zeros((height, width, 3), dtype='uint8')
-cv2.polylines(initImg, [pts], True, (0, 0, 255), thickness=5)
+if len(sys.argv)==2:
+    ### Reads image from a file
+    img = cv2.imread(sys.argv[1], cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = rotate(img, 30, mode="edge")
+    height, width, depth = img.shape
 
-### Reads image from a file
-img = cv2.imread("resistor-blue.jpg", cv2.IMREAD_COLOR)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img = rotate(img, 30, mode="edge")
-cropRectangle(img, init)
+    init = initializeEllipticSnake(height, width)
+    cropRectangle(img, init)
+    edgeMath("output.jpg")
 
+else:
+    cap = cv2.VideoCapture(0)
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+    init = initializeEllipticSnake(height, width)
+    pts = init.reshape((-1, 1, 2))
+    initImg = np.zeros((height, width, 3), dtype='uint8')
+    cv2.polylines(initImg, [pts], True, (0, 0, 255), thickness=5)
 
-while(True):
-    ### Reads image from the computer camera
-    # ret, img = cap.read()
-    # cv2.imshow("Camera", cv2.add(initImg, img))
+    while(True):
+        ### Reads image from the computer camera
+        ret, img = cap.read()
+        cv2.imshow("Camera", cv2.add(initImg, img))
 
-    k = cv2.waitKey(1)
-    if k & 0xFF == ord('q'):
-        break
-    elif k & 0xFF == ord('e'):
-        ### Convert to rgb since IMREAD_COLOR does bgr
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        cropRectangle(img, init)
-cap.release()
-cv2.destroyAllWindows()
+        k = cv2.waitKey(1)
+        if k & 0xFF == ord('q'):
+            break
+        elif k & 0xFF == ord('e'):
+            ### Convert to rgb since IMREAD_COLOR does bgr
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            cropRectangle(img, init)
+            edgeMath("output.jpg")
+
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ resistor_utilities.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+def initializeEllipticSnake(height, width):
+    s = np.linspace(0, 2*np.pi, 400)
+    x = width/2 + width/3*np.cos(s)
+    y = height/2 + height/3*np.sin(s)
+    init = np.array([x, y]).T
+    init = init.astype(int)
+    return init
+
+def findColor(rgb_values):
+    print (rgb_values)
+    #Black
+    if (rgb_values[0] >= 0 and rgb_values[0] < 15 and rgb_values[1] >= 0 and rgb_values[1] < 15 and rgb_values[2] >= 0 and rgb_values[2] < 15):
+        print ("black")
+    #Brown
+    elif (rgb_values[0] >= 75 and rgb_values[0] <= 105 and rgb_values[1] >= 30 and rgb_values[1] <= 60 and rgb_values[2] >= 30 and rgb_values[2] <= 60):
+        print ("brown")
+    #Red
+    elif (rgb_values[0] >= 240 and rgb_values[0] <= 255 and rgb_values[1] >= 0 and rgb_values[1] < 15 and rgb_values[2] >= 0 and rgb_values[2] < 15):
+        print ("red")
+    #Orange
+    elif (rgb_values[0] >= 240 and rgb_values[0] <= 255 and rgb_values[1] >= 112 and rgb_values[1] <= 142 and rgb_values[2] >= 0 and rgb_values[2] <= 15):
+        print ("orange")
+    #Yellow
+    elif (rgb_values[0] >= 240 and rgb_values[0] <= 255 and rgb_values[1] >= 240 and rgb_values[1] <= 255 and rgb_values[2] >= 0 and rgb_values[2] <= 15):
+        print ("yellow")
+    #Green
+    elif (rgb_values[0] >= 0 and rgb_values[0] <= 15 and rgb_values[1] >= 240 and rgb_values[1] <= 255 and rgb_values[2] >= 0 and rgb_values[2] <= 15):
+        print ("green")
+    #Blue
+    elif (rgb_values[0] >= 0 and rgb_values[0] <= 15 and rgb_values[1] >= 0 and rgb_values[1] <= 15 and rgb_values[2] >= 240 and rgb_values[2] <= 255):
+        print ("blue")
+    #Violet
+    elif (rgb_values[0] >= 165 and rgb_values[0] <= 195 and rgb_values[1] >= 55 and rgb_values[1] <= 85 and rgb_values[2] >= 205 and rgb_values[2] <= 235):
+        print ("violet")
+    #Grey
+    elif (rgb_values[0] >= 142 and rgb_values[0] <= 172 and rgb_values[1] >= 132 and rgb_values[1] <= 162 and rgb_values[2] >= 145 and rgb_values[2] <= 175):
+        print ("grey")
+    #White
+    elif (rgb_values[0] >= 240 and rgb_values[0] <= 255 and rgb_values[1] >= 240 and rgb_values[1] <= 255 and rgb_values[2] >= 240 and rgb_values[2] <= 255):
+        print ("white")
+    else:
+        print ("not detected")
+    return;
+    
